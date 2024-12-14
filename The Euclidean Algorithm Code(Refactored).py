4457d7e6d785f37e03e8411ea6317241475cf44e
Hydro <hydro.xd10@gmail.com>
2024-12-14 20:48:06 +0000

    changed the factored code to put it in appropriate class

--- The Euclidean Algorithm Code(Refactored).py
+++ The Euclidean Algorithm Code(Refactored).py
@@ -1,21 +1,23 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Dec 13 23:24:28 2024
 
-@author: hydro
-"""
+class Math:
+
+    def find_the_greatest_common_factor(Num1,Num2):# find the greatest common factor between two numbers   
+            if Num1==0:
+                return Num2# if one of the numbers is zero so the greatest common factor is the other number
+            if Num2==0:
+                return Num1
+            
+            else: # if non of the numbers is zero
+            
+                The_remainder=Num1%Num2
+                return Math.find_the_greatest_common_factor(Num2,The_remainder)
+'''
+from line 10 to 13
+
+this will continue until the remainder equal zero so the other number will be the greatest common factor following the euclidean algorithm
+'''
 
 
-def find_the_greatest_common_factor(Num1,Num2):# find the greatest common factor between two numbers   
-        if Num1==0:
-            return Num2# if one of the numbers is zero so the freatest common factor is the other number
-        if Num2==0:
-            return Num1
+result=Math.find_the_greatest_common_factor(22, 2)
         
-        else: # if non of the numbers is zero
-        
-            The_remainder=Num1%Num2
-            return find_the_greatest_common_factor(Num2,The_remainder)# get the remain and Num2/the remain and this will contune until the remain is zero
-        
-    
-print(find_the_greatest_common_factor(270, 192))
+print(result)
